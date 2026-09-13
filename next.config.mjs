7628b7c919b20c321a75723b79b46ecbdab4bfc5
Ravi-nu <ravinarayanau2@gmail.com>
2026-09-13 13:35:28 +0530

    fix(css): create tailwind.config.js content scanner, update postcss config, and resolve dev server static asset 404s

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images:{
-        domains: ['lh3.googleusercontent.com'],
-    }
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
